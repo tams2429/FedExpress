3d6754becc8a45e3f763b0deda62b1c93b2a8e64
Siu Kei Tam <tam.s.k.92@gmail.com>
2021-01-16 16:55:53 +0000

    Added template Section divider pages with titles

--- frontEnd/src/components/TheBeginning.js
+++ frontEnd/src/components/TheBeginning.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const TheBeginning = () => {
   return (
-    <div className='the-beginning slide'></div>
+    <div className='the-beginning slide'>
+      <div className='LHS-container'>
+        <p>THE</p>
+        <p>BEGINNING</p>
+      </div>
+      <div className='RHS-container'>
+        <div className='Top-RHS-container'></div>
+        <div className='Bottom-RHS-container'></div>
+      </div>
+    </div>
   )
 }
 
