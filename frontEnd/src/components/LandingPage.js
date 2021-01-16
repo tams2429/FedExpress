122cbfcd1fd4ad170f684cedc3e0d605a8a37499
Siu Kei Tam <tam.s.k.92@gmail.com>
2021-01-16 16:17:02 +0000

    Updated background and styling for Landing Page

--- frontEnd/src/components/LandingPage.js
+++ frontEnd/src/components/LandingPage.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div className='landing-page slide'></div>
+    <div className='landing-page slide'>
+      <div className='title-container'>
+        <h1>Roger Federer</h1>
+      </div>
+      <div className='quote-container'>
+        <q>There is no way around hard work. Embrace it.</q>
+      </div>
+    </div>
   )
 }
 
