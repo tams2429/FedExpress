d099c20da7d89c16e57eb6dc9d8f2123bf1c82a1
Siu Kei Tam <tam.s.k.92@gmail.com>
2021-01-16 12:28:30 +0000

    Setup of initial files and testing components are rendering

---
+++ frontEnd/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='outer-wrapper'>
+      <div className='wrapper'>
+        <div className='landing-page slide'></div>
+        <div className='the-beginning slide'></div>
+        <div className='beginning-page slide'></div>
+        <div className='tennis-page slide'></div>
+        <div className='the-professional slide'></div>
+        <div className='professional-page slide'></div>
+        <div className='the-philanthropist slide'></div>
+        <div className='philanthropist-page slide'></div>
+        <div className='the-champion slide'></div>
+        <div className='champion-page slide'></div>
+        <div className='the-husband-and-father slide'></div>
+        <div className='husband-and-father-page slide'></div>
+        <div className='the-legend slide'></div>
+        <div className='legend-page slide'></div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
